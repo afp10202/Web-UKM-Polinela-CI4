483fd351d1624f9427a59649b28b14382df9469e
kekeputri08 <113098935+kekeputri08@users.noreply.github.com>
2023-07-07 15:54:40 +0700

    Create about.php
    tentang kami

---
+++ app/Views/about.php
@@ -0,0 +1,18 @@
+<?= $this->extend('layout/page_layout') ?>
+
+<?= $this->section('content') ?>
+
+<div class="container">
+    <h1>Tentang Kami</h1>
+    <p>Selamat datang di halaman "Informasi UKM" kami! Di sini, kami dengan bangga 
+         mempersembahkan berbagai Unit Kegiatan Mahasiswa (UKM) yang aktif dan beragam di kampus kami.</p>
+    <p>UKM merupakan wadah bagi para mahasiswa untuk mengembangkan minat, bakat, 
+         dan keterampilan di luar kegiatan akademik.</p>
+    <p>Kami memiliki beberapa UKM yang menawarkan berbagai kesempatan yang menarik bagi mahasiswa. 
+        Apakah Anda berminat dalam olahraga, seni,atau mungkin lebih tertarik pada kegiatan alam? </p>
+    <p>Di sini, kami memiliki UKM yang dapat memenuhi kebutuhan Anda. Setiap UKM dikelola oleh tim mahasiswa yang berdedikasi dan berpengalaman, 
+        siap memberikan bimbingan, dukungan, serta kesempatan berharga bagi anggotanya.</p>
+    <p>Terima kasih telah memilih Web UKM POLINELA sebagai platform streaming pilihan Anda. Never Stop!!!</p>
+</div>
+
+<?= $this->endSection() ?>
